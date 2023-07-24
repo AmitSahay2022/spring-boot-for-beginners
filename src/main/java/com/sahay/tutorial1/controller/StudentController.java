c609f8fec01e4d7b967eed3f6e74cebd12115418
Sahay Software <sahay.mtech2013@gmail.com>
2023-07-24 17:18:56 +0530

    Updated

--- src/main/java/com/sahay/tutorial1/controller/StudentController.java
+++ src/main/java/com/sahay/tutorial1/controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.sahay.tutorial1.controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -8,13 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sahay.tutorial1.entity.Student;
+
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
 
 	@GetMapping 
-	public ResponseEntity<List<String>> findAllStudents(){
-		List<String> students = List.of("Amit","Sumit","Leah","Sunny");
-		return new ResponseEntity<List<String>>(students,HttpStatus.OK);
+	public ResponseEntity<List<Student>> findAllStudents(){
+		List<Student> students = List.of(new Student("Amit", "Kumar", LocalDate.now(), "sahay.mtech2013@gmail.com", 35),
+		new Student("Sumit", "Kumar", LocalDate.now(), "mba.2012@gmail.com", 30),
+		new Student("Sunny", "Leone", LocalDate.now(), "leone.sexy2013@gmail.com", 20));
+		return new ResponseEntity<List<Student>>(students,HttpStatus.OK);
 	}
 }
